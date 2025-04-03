a8b208f600c4b9dbb42f4aa6b3ee4e08704b9241
Kevin Lopes Costa <kevinlopes3012@gmail.com>
2025-04-03 10:58:03 -0400

    feat: add integration test

---
+++ 17_IntroToIntegrationTests/src/main/java/br/com/Kevinlps/unittests/services/PersonServices.java
@@ -0,0 +1,94 @@
+package br.com.Kevinlps.services;
+
+import br.com.Kevinlps.controllers.PersonController;
+import br.com.Kevinlps.data.dto.PersonDTO;
+import br.com.Kevinlps.exception.RequiredObjectIsNullException;
+import br.com.Kevinlps.model.Person;
+import br.com.Kevinlps.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+
+import static br.com.Kevinlps.mapper.ObjctMapper.parseListObjects;
+import static br.com.Kevinlps.mapper.ObjctMapper.parseObject;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Service
+public class PersonServices {
+
+    private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    PersonRepository repository;
+
+
+    public List<PersonDTO> findAll() {
+
+        logger.info("Finding all People!");
+
+        var persons = parseListObjects(repository.findAll(), PersonDTO.class);
+        persons.forEach(this::addHateoasLink);
+        return persons;
+    }
+
+    public PersonDTO findById(Long id) {
+        logger.info("Finding one Person!");
+
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new RequiredObjectIsNullException("No records found for this ID!"));
+        var dto =  parseObject(entity, PersonDTO.class);
+        addHateoasLink( dto);
+        return dto;
+    }
+
+
+
+    public PersonDTO create(PersonDTO person) {
+
+        if (person == null) throw new RequiredObjectIsNullException();
+
+        logger.info("Creating one Person!");
+        var entity = parseObject(person, Person.class);
+
+        var dto  = parseObject(repository.save(entity), PersonDTO.class);
+        addHateoasLink( dto);
+        return dto;
+    }
+
+    public PersonDTO update(PersonDTO person) {
+
+        if (person == null) throw new RequiredObjectIsNullException();
+
+        logger.info("Updating one Person!");
+        Person entity = repository.findById(person.getId())
+                .orElseThrow(() -> new RequiredObjectIsNullException("No records found for this ID!"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        var dto = parseObject(repository.save(entity), PersonDTO.class);
+        addHateoasLink( dto);
+        return dto;
+    }
+
+    public void deleteById(Long id) {
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new RequiredObjectIsNullException("No records found for this ID"));
+        repository.delete(entity);
+    }
+    private void addHateoasLink( PersonDTO dto) {
+        dto.add(linkTo(methodOn(PersonController.class).findById(dto.getId())).withSelfRel().withType("GET"));
+        dto.add(linkTo(methodOn(PersonController.class).deleteById(dto.getId())).withRel("delete").withType("DELETE"));
+        dto.add(linkTo(methodOn(PersonController.class).findAll()).withRel("findAll").withType("GET"));
+        dto.add(linkTo(methodOn(PersonController.class).create(dto)).withRel("create").withType("POST"));
+        dto.add(linkTo(methodOn(PersonController.class).update(dto)).withRel("update").withType("PUT"));
+    }
+}
---
+++ 18_ConfiguringCORS/src/main/java/br/com/Kevinlps/controllers/PersonController.java
@@ -0,0 +1,79 @@
+package br.com.Kevinlps.controllers;
+
+
+import br.com.Kevinlps.controllers.docs.PersonControllerDocs;
+import br.com.Kevinlps.data.dto.PersonDTO;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints for Managing People")
+public class PersonController implements PersonControllerDocs {
+
+    @Autowired
+    private br.com.Kevinlps.services.PersonServices service;
+
+    @GetMapping(produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
+    @Override
+    public List<PersonDTO> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping(value = "/{id}",
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE}
+    )
+    @Override
+    public PersonDTO findById(@PathVariable("id") Long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping(
+            consumes = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE},
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE}
+    )
+    @Override
+    public PersonDTO create(@RequestBody PersonDTO person) {
+        return service.create(person);
+    }
+
+    @PutMapping(
+            consumes = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE},
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE}
+    )
+    @Override
+    public PersonDTO update(@RequestBody PersonDTO person) {
+        return service.update(person);
+    }
+
+
+    @DeleteMapping(value = "/{id}")
+    @Override
+    public ResponseEntity<?> deleteById(@PathVariable("id") Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
