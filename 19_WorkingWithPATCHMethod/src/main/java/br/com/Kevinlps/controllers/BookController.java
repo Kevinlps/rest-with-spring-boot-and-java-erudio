8c9bcf842eab1bd1764d7e2a51f783196f73033d
Kevin Lopes Costa <kevinlopes3012@gmail.com>
2025-04-04 10:19:26 -0400

    feat: add cors

--- 19_WorkingWithPATCHMethod/src/main/java/br/com/Kevinlps/controllers/BookController.java
+++ 19_WorkingWithPATCHMethod/src/main/java/br/com/Kevinlps/controllers/BookController.java
@@ -1,8 +1,8 @@
 package br.com.Kevinlps.controllers;
 
 
-import br.com.Kevinlps.controllers.docs.PersonControllerDocs;
-import br.com.Kevinlps.data.dto.PersonDTO;
+import br.com.Kevinlps.controllers.docs.BookControllerDocs;
+import br.com.Kevinlps.data.dto.BookDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -11,21 +11,20 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@CrossOrigin(origins = "http://localhost:8080")
 @RestController
-@RequestMapping("/api/person/v1")
-@Tag(name = "People", description = "Endpoints for Managing People")
-public class PersonController implements PersonControllerDocs {
+@RequestMapping("/api/book/v1")
+@Tag(name = "Book", description = "Endpoints for Managing Book")
+public class BookController implements BookControllerDocs {
 
     @Autowired
-    private br.com.Kevinlps.services.PersonServices service;
+    private br.com.Kevinlps.services.BookServices service;
 
     @GetMapping(produces = {
             MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE,
             MediaType.APPLICATION_YAML_VALUE})
     @Override
-    public List<PersonDTO> findAll() {
+    public List<BookDTO> findAll() {
         return service.findAll();
     }
 
@@ -36,9 +35,10 @@ public class PersonController implements PersonControllerDocs {
                     MediaType.APPLICATION_YAML_VALUE}
     )
     @Override
-    public PersonDTO findById(@PathVariable("id") Long id) {
+    public BookDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
+
     @PostMapping(
             consumes = {
                     MediaType.APPLICATION_JSON_VALUE,
@@ -50,8 +50,8 @@ public class PersonController implements PersonControllerDocs {
                     MediaType.APPLICATION_YAML_VALUE}
     )
     @Override
-    public PersonDTO create(@RequestBody PersonDTO person) {
-        return service.create(person);
+    public BookDTO create(@RequestBody BookDTO book) {
+        return service.create(book);
     }
 
     @PutMapping(
@@ -65,8 +65,8 @@ public class PersonController implements PersonControllerDocs {
                     MediaType.APPLICATION_YAML_VALUE}
     )
     @Override
-    public PersonDTO update(@RequestBody PersonDTO person) {
-        return service.update(person);
+    public BookDTO update(@RequestBody BookDTO book) {
+        return service.update(book);
     }
 
 
