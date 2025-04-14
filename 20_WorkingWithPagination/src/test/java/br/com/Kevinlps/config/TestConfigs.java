0e32848a127d8d3b00b5a3c459e26551a342c369
Kevin Lopes Costa <kevinlopes3012@gmail.com>
2025-04-14 11:38:28 -0400

    feat: impl pagination search and tests

---
+++ 20_WorkingWithPagination/src/test/java/br/com/Kevinlps/config/TestConfigs.java
@@ -0,0 +1,13 @@
+package br.com.Kevinlps.config;
+
+public interface TestConfigs {
+    int SERVER_PORT = 8888;
+
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+
+    String ORIGIN_ERUDIO = "http://localhost:8080";
+    String ORIGIN_SEMERU = "http://www.semeru.com.br";
+
+
+}
